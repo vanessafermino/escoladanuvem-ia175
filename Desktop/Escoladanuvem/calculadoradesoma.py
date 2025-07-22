66d0cab4d153a00a9936a0226b28aabd0a5b8c12
Vanessa <vanessabrigit@yahoo.com.br>
2025-07-22 20:36:46 -0300

    Delete Desktop directory

--- Desktop/Escoladanuvem/calculadoradesoma.py
+++
@@ -1,9 +0,0 @@
-# Define as duas variáveis com os números a serem somados
-numero1 = 12
-numero2 = 14
-
-# Realiza a soma das duas variáveis e armazena o resultado em uma nova variável
-soma = numero1 + numero2
-
-# Imprime o resultado da soma na tela
-print(soma)
